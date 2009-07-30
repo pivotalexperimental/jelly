eb2985a5dcf673b79905f79326934951f7cc0478
Nate Clark <nate@pivotallabs.com>
2009-07-30 13:42:37 -0700

    fixing generator

--- generators/jelly/jelly_generator.rb
+++ generators/jelly/jelly_generator.rb
@@ -1,10 +1,10 @@
-class JellyGenerator < Rails::Generator::NamedBase
+class JellyGenerator < Rails::Generator::Base
   def manifest
     record do |m|
-      m.file 'jelly.js', "public/javascripts/jelly.js"
-      m.file 'ajax_with_jelly.js', "public/javascripts/ajax_with_jelly.js"
-      m.file 'jquery/jquery-1.3.2.js', "public/javascripts/jquery-1.3.2.js"
-      m.file 'jquery/jquery.protify-0.3.js', "public/javascripts/jquery.protify-0.3.js"
+      m.file 'javascripts/jelly.js', "public/javascripts/jelly.js"
+      m.file 'javascripts/ajax_with_jelly.js', "public/javascripts/ajax_with_jelly.js"
+      m.file 'javascripts/jquery/jquery-1.3.2.js', "public/javascripts/jquery/jquery-1.3.2.js"
+      m.file 'javascripts/jquery/jquery.protify-0.3.js', "public/javascripts/jquery/jquery.protify-0.3.js"
     end
   end
 end
