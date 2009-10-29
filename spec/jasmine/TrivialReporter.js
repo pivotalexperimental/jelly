c39d4b06c279ae3a77331d58f584e3f52c417b2f
Nate Clark & Sam Pierson <opensource@pivotallabs.com>
2009-10-28 17:57:39 -0700

    updating Jasmine to 0.10.0

--- spec/jasmine/TrivialReporter.js
+++ spec/jasmine/TrivialReporter.js
@@ -1,4 +1,6 @@
-jasmine.TrivialReporter = function() {
+jasmine.TrivialReporter = function(doc) {
+  this.document = doc || document;
+  this.suiteDivs = {};
 };
 
 jasmine.TrivialReporter.prototype.createDom = function(type, attrs, childrenVarArgs) {
@@ -10,50 +12,110 @@ jasmine.TrivialReporter.prototype.createDom = function(type, attrs, childrenVarA
     if (typeof child === 'string') {
       el.appendChild(document.createTextNode(child));
     } else {
-      el.appendChild(child);
+      if (child) { el.appendChild(child); }
     }
   }
 
   for (var attr in attrs) {
-    if (attr == 'className') {
-      el.setAttribute('class', attrs[attr]);
-    } else {
       el[attr] = attrs[attr];
-    }
   }
 
   return el;
 };
 
+jasmine.TrivialReporter.prototype.reportRunnerStarting = function(runner) {
+  var suites = runner.suites();
+
+  this.runnerDiv = this.createDom('div', { className: 'runner running' },
+      this.createDom('a', { className: 'run_spec', href: '?' }, "run all"),
+      this.runnerMessageSpan = this.createDom('span', {}, "Running..."));
+  this.document.body.appendChild(this.runnerDiv);
+
+  for (var i = 0; i < suites.length; i++) {
+    var suite = suites[i];
+    var suiteDiv = this.createDom('div', { className: 'suite' },
+        this.createDom('a', { className: 'run_spec', href: '?spec=' + encodeURIComponent(suite.getFullName()) }, "run"),
+        this.createDom('a', { className: 'description', href: '?spec=' + encodeURIComponent(suite.getFullName()) }, suite.description));
+    this.suiteDivs[suite.getFullName()] = suiteDiv;
+    var parentDiv = this.document.body;
+    if (suite.parentSuite) {
+      parentDiv = this.suiteDivs[suite.parentSuite.getFullName()];
+    }
+    parentDiv.appendChild(suiteDiv);
+  }
+
+  this.startedAt = new Date();
+};
+
 jasmine.TrivialReporter.prototype.reportRunnerResults = function(runner) {
-  console.log(runner);
+  var results = runner.results();
+  var className = (results.failedCount > 0) ? "runner failed" : "runner passed";
+  this.runnerDiv.setAttribute("class", className);
+  //do it twice for IE
+  this.runnerDiv.setAttribute("className", className);
+  var specs = runner.specs();
+  var specCount = 0;
+  for (var i = 0; i < specs.length; i++) {
+    if (this.specFilter(specs[i])) {
+      specCount++;
+    }
+  }
+  var message = "" + specCount + " spec" + (specCount == 1 ? "" : "s" ) + ", " + results.failedCount + " failure" + ((results.failedCount == 1) ? "" : "s");
+  message += " in " + ((new Date().getTime() - this.startedAt.getTime()) / 1000) + "s";
+  this.runnerMessageSpan.replaceChild(this.createDom('a', { className: 'description', href: '?'}, message), this.runnerMessageSpan.firstChild);
 };
 
 jasmine.TrivialReporter.prototype.reportSuiteResults = function(suite) {
-  console.log(suite);
+  var results = suite.results();
+  var status = results.passed() ? 'passed' : 'failed';
+  if (results.totalCount == 0) { // todo: change this to check results.skipped
+    status = 'skipped';
+  }
+  this.suiteDivs[suite.getFullName()].className += " " + status;
 };
 
 jasmine.TrivialReporter.prototype.reportSpecResults = function(spec) {
-  var specDiv = this.createDom('div', {
-    className: spec.getResults().passed ? 'spec passed' : 'spec failed'
-  }, spec.getFullName());
+  var results = spec.results();
+  var status = results.passed() ? 'passed' : 'failed';
+  if (results.skipped) {
+    status = 'skipped';
+  }
+  var specDiv = this.createDom('div', { className: 'spec '  + status },
+      this.createDom('a', { className: 'run_spec', href: '?spec=' + encodeURIComponent(spec.getFullName()) }, "run"),
+      this.createDom('a', { className: 'description', href: '?spec=' + encodeURIComponent(spec.getFullName()) }, spec.getFullName()));
 
-  var resultItems = spec.getResults().getItems();
+
+  var resultItems = results.getItems();
   for (var i = 0; i < resultItems.length; i++) {
     var result = resultItems[i];
-    if (!result.passed) {
+    if (result.passed && !result.passed()) {
       var resultMessageDiv = this.createDom('div', {className: 'resultMessage fail'});
       resultMessageDiv.innerHTML = result.message; // todo: lame; mend
       specDiv.appendChild(resultMessageDiv);
       specDiv.appendChild(this.createDom('div', {className: 'stackTrace'}, result.trace.stack));
     }
   }
-
-  document.body.appendChild(specDiv);
+  this.suiteDivs[spec.suite.getFullName()].appendChild(specDiv);
 };
 
 jasmine.TrivialReporter.prototype.log = function() {
   console.log.apply(console, arguments);
+};
+
+jasmine.TrivialReporter.prototype.getLocation = function() {
+  return this.document.location;
+};
+
+jasmine.TrivialReporter.prototype.specFilter = function(spec) {
+  var paramMap = {};
+  var params = this.getLocation().search.substring(1).split('&');
+  for (var i = 0; i < params.length; i++) {
+    var p = params[i].split('=');
+    paramMap[decodeURIComponent(p[0])] = decodeURIComponent(p[1]);
+  }
+
+  if (!paramMap["spec"]) return true;
+  return spec.getFullName().indexOf(paramMap["spec"]) == 0;
 };
 
 //protect against console.log incidents
