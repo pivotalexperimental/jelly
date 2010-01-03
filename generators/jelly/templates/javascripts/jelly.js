5201d4f4c557e2eca8adf9de3a2f0a30e80bd725
Brian Takita <brian@honk.com>
2010-01-03 13:53:01 -0800

    Reformatted Jelly definition.

--- generators/jelly/templates/javascripts/jelly.js
+++ generators/jelly/templates/javascripts/jelly.js
@@ -20,130 +20,132 @@ if (!Function.prototype.bind) {
     }
   }
 }
-Jelly.init = function() {
-  this.observers = [];
-  this.attach = this.Observers.attach;
-  this.notifyObservers = this.Observers.notify;
-  this.Pages.init();
-};
+$.extend(Jelly, {
+  init: function() {
+    this.observers = [];
+    this.attach = this.Observers.attach;
+    this.notifyObservers = this.Observers.notify;
+    this.Pages.init();
+  },
 
-Jelly.Observers = {
-  attach: function() {
-    if (this == Jelly) {
-      return Jelly.Observers.attach.apply(this.observers, arguments);
-    }
-    for (var i = 0; i < arguments.length; i++) {
-      var definitionOrComponent = arguments[i];
-      var observer;
-      if (definitionOrComponent.component) {
-        var component = Jelly.Observers.evaluateComponent(definitionOrComponent.component);
-        if (component.init) {
-          observer = component.init.apply(component, definitionOrComponent.arguments) || component;
+  Observers: {
+    attach: function() {
+      if (this == Jelly) {
+        return Jelly.Observers.attach.apply(this.observers, arguments);
+      }
+      for (var i = 0; i < arguments.length; i++) {
+        var definitionOrComponent = arguments[i];
+        var observer;
+        if (definitionOrComponent.component) {
+          var component = Jelly.Observers.evaluateComponent(definitionOrComponent.component);
+          if (component.init) {
+            observer = component.init.apply(component, definitionOrComponent.arguments) || component;
+          } else {
+            observer = component;
+          }
         } else {
-          observer = component;
+          observer = Jelly.Observers.evaluateComponent(definitionOrComponent);
         }
-      } else {
-        observer = Jelly.Observers.evaluateComponent(definitionOrComponent);
+        this.push(observer);
       }
-      this.push(observer);
-    }
-  },
+    },
 
-  evaluateComponent: function(component) {
-    return (typeof component == "string") ? eval(component) : component;
-  },
+    evaluateComponent: function(component) {
+      return (typeof component == "string") ? eval(component) : component;
+    },
 
-  notify: function(callbacks) {
-    if (this == Jelly) {
-      return Jelly.Observers.notify.apply(this.observers, arguments);
-    }
-    if (!$.isArray(callbacks)) {
-      callbacks = [callbacks];
-    }
-
-    var observers = this.slice(0);
-    for (var i = 0; i < callbacks.length; i++) {
-      var callback = callbacks[i];
-
-      // Deprecate 'on' in favor of making each page action a Component.
-      if (callback.on) {
-        var additionalObserver = eval(callback.on);
-        if (observers.indexOf(additionalObserver) == -1) {
-          observers.push(additionalObserver);
-        }
+    notify: function(callbacks) {
+      if (this == Jelly) {
+        return Jelly.Observers.notify.apply(this.observers, arguments);
+      }
+      if (!$.isArray(callbacks)) {
+        callbacks = [callbacks];
       }
 
-      if (callback.method) {
-        for (var j = 0; j < observers.length; j++) {
-          var observer = observers[j];
-          if (observer[callback.method]) {
-            if (observer.detach && observer.detach()) {
-              Jelly.Observers.garbageCollectObserver.call(this, observer);
-            } else {
-              observer[callback.method].apply(observer, callback.arguments);
+      var observers = this.slice(0);
+      for (var i = 0; i < callbacks.length; i++) {
+        var callback = callbacks[i];
+
+        // Deprecate 'on' in favor of making each page action a Component.
+        if (callback.on) {
+          var additionalObserver = eval(callback.on);
+          if (observers.indexOf(additionalObserver) == -1) {
+            observers.push(additionalObserver);
+          }
+        }
+
+        if (callback.method) {
+          for (var j = 0; j < observers.length; j++) {
+            var observer = observers[j];
+            if (observer[callback.method]) {
+              if (observer.detach && observer.detach()) {
+                Jelly.Observers.garbageCollectObserver.call(this, observer);
+              } else {
+                observer[callback.method].apply(observer, callback.arguments);
+              }
             }
           }
         }
+
+        if (callback.attach) {
+          Jelly.Observers.attach.apply(this, callback.attach);
+        }
       }
+    },
 
-      if (callback.attach) {
-        Jelly.Observers.attach.apply(this, callback.attach);
+    garbageCollectObserver: function(observer) {
+      var index = this.indexOf(observer);
+      if (index > -1) {
+        Jelly.Observers.remove.call(this, index, index + 1);
       }
+    },
+
+    remove: function(from, to) {
+      var rest = this.slice((to || from) + 1 || this.length);
+      this.length = from < 0 ? this.length + from : from;
+      return this.push.apply(this, rest);
     }
   },
 
-  garbageCollectObserver: function(observer) {
-    var index = this.indexOf(observer);
-    if (index > -1) {
-      Jelly.Observers.remove.call(this, index, index + 1);
+  Pages: {
+    init: function() {
+      this.all = {};
+      Jelly.all = this.all; // Deprecated
+    },
+
+    add: function(name) {
+      var page = new Jelly.Page.Constructor(name);
+      for (var i = 1; i < arguments.length; i++) {
+        $.extend(page, arguments[i]);
+      }
+      return page;
     }
   },
 
-  remove: function(from, to) {
-    var rest = this.slice((to || from) + 1 || this.length);
-    this.length = from < 0 ? this.length + from : from;
-    return this.push.apply(this, rest);
+  Page: {
+    init: function(controllerName, actionName) {
+      var page = Jelly.Pages.all[controllerName] || new Jelly.Page.Constructor(controllerName);
+      window.page = page;
+      if (page.all) page.all();
+      if (page[actionName]) page[actionName].call(page);
+      page.loaded = true;
+      return page;
+    },
+    Constructor: function(name) {
+      this.loaded = false;
+      this.documentHref = Jelly.Location.documentHref;
+
+      this.name = name;
+      Jelly.Pages.all[name] = this;
+    }
+  },
+
+  Location: {
+    on_redirect: function(location) {
+      top.location.href = location;
+    }
   }
-};
-
-Jelly.Pages = {
-  init: function() {
-    this.all = {};
-    Jelly.all = this.all; // Deprecated
-  },
-
-  add: function(name) {
-    var page = new Jelly.Page.Constructor(name);
-    for (var i = 1; i < arguments.length; i++) {
-      $.extend(page, arguments[i]);
-    }
-    return page;
-  }
-};
+});
 Jelly.add = Jelly.Pages.add; // Deprecated
-
-Jelly.Page = {
-  init: function(controllerName, actionName) {
-    var page = Jelly.Pages.all[controllerName] || new Jelly.Page.Constructor(controllerName);
-    window.page = page;
-    if (page.all) page.all();
-    if (page[actionName]) page[actionName].call(page);
-    page.loaded = true;
-    return page;
-  },
-  Constructor: function(name) {
-    this.loaded = false;
-    this.documentHref = Jelly.Location.documentHref;
-
-    this.name = name;
-    Jelly.Pages.all[name] = this;
-  }
-};
-
-Jelly.Location = {
-  on_redirect: function(location) {
-    top.location.href = location;
-  }
-};
 
 Jelly.init();
