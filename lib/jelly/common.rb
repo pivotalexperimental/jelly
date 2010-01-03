7540d502749d745ebeeba65cd4a3f46b54c5158d
Brian Takita <brian@honk.com>
2010-01-03 13:57:02 -0800

    Removed unused method.

--- lib/jelly/common.rb
+++ lib/jelly/common.rb
@@ -4,10 +4,6 @@ module Jelly
       {"method" => method, "arguments" => arguments}
     end
 
-    def jelly_attach_hash(*)
-      {"method" => method, "arguments" => arguments}
-    end
-
     def jelly_attach_component_definition_hash(component_name, *args)
       {'component' => component_name, 'arguments' => args}
     end
