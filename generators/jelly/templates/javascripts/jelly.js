adb46f9004318cf796a294b902d8d422295940b9
Brian Takita <brian@honk.com>
2010-01-08 14:29:26 -0800

    bump

--- generators/jelly/templates/javascripts/jelly.js
+++ generators/jelly/templates/javascripts/jelly.js
@@ -2,7 +2,7 @@
  *  Jelly. a sweet unobtrusive javascript framework
  *  for jQuery and Rails
  *
- *  version 0.8.9
+ *  version 0.8.10
  *
  * Copyright (c) 2009 Pivotal Labs
  * Licensed under the MIT license.
